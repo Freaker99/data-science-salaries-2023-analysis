c84d1e500afa3037177c3a1c49ba6bac7870b537
Aleksander Kostecki <68340870+Freaker99@users.noreply.github.com>
2023-06-12 20:31:30 +0200

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,13 +1,22 @@
 CREATE DATABASE DataScienceSalaries DEFAULT CHARACTER SET utf8;
 USE DataScienceSalaries;
 
+-- Display all data
 SELECT *
+FROM datasciencesalaries.ds_salaries;
+SELECT COUNT(*) FROM datasciencesalaries.ds_salaries;
+
+-- Select data that we are going to be using
+SELECT job_title, salary_in_usd, remote_ratio
 FROM datasciencesalaries.ds_salaries
-ORDER BY 4
+ORDER BY job_title;
 
--- Select Data that we are going to be using
-
-SELECT job_title, salary, salary_currency, salary_in_usd, remote_ratio
+-- Show highest salary in USD of each job title (including all experience levels)
+SELECT job_title, MAX(salary_in_usd) as highest_salary_in_usd
 FROM datasciencesalaries.ds_salaries
-ORDER BY 1, 4 DESC
+GROUP BY job_title;
 
+-- Show average salary in USD of each job title (including all experience levels)
+SELECT job_title, ROUND(AVG(salary_in_usd), 2) as average_salary_in_usd
+FROM datasciencesalaries.ds_salaries
+GROUP BY job_title;
